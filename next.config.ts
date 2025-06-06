fb1914ab6a6f583a3600ec968d1cc741d76bc96c
stanley <stanleygomesdasilva@gmail.com>
2025-06-06 15:02:20 -0300

    chore: ajuste config next

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  /* config options here */
+  output: 'standalone',
   images: {
     unoptimized: true,
   },
