254590a9c1fd2fbc359996b2ad76115d8e7c346e
stanley <stanleygomesdasilva@gmail.com>
2025-06-13 11:23:49 -0300

    chore: ajustar espaco abaixo do tabbar

--- src/shared/ui/tab-bar/TabBar.tsx
+++ src/shared/ui/tab-bar/TabBar.tsx
@@ -7,7 +7,7 @@ interface TabBarProps {
 
 const TabBar: React.FC<TabBarProps> = ({ children, className }) => (
   <nav
-    className={`fixed bottom-0 left-0 w-full z-50 bg-white/80 dark:bg-black/80 border-t border-gray-200 dark:border-gray-800 flex justify-around py-2 shadow-[0_-4px_16px_-4px_rgba(0,0,0,0.10)] backdrop-blur ${className || ""}`}
+    className={`fixed bottom-0 left-0 w-full z-50 bg-white/80 dark:bg-black/80 border-t border-gray-200 dark:border-gray-800 flex justify-around py-2 shadow-[0_-4px_16px_-4px_rgba(0,0,0,0.10)] backdrop-blur tabbar-bottom-safe-area ${className || ""}`}
   >
     {children}
   </nav>
