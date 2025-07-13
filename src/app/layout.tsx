f768281c38ccaaecb6c0f2241b353a907ee95109
stanley <stanleygomesdasilva@gmail.com>
2025-07-13 06:05:50 -0300

    chore: mudança pasta de estilo global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Viewport, Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../style/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +19,6 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  // themeColor: 'black',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: '#ffffff' },
     { media: '(prefers-color-scheme: dark)', color: '#363030' },
