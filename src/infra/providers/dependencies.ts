559e5fd2a1863bc3e8f46259a0f1785cccaba273
stanley <stanleygomesdasilva@gmail.com>
2025-07-24 10:18:23 -0300

    feat: movendo o controle de token do mongo para redis

--- src/infra/providers/dependencies.ts
+++ src/infra/providers/dependencies.ts
@@ -1,14 +1,13 @@
 import { GetUrlConsentUseCase } from '../../application/usecases/get-url-consent-use-case.js';
 import { GetUrlRedirectBackUseCase } from '../../application/usecases/get-url-redirect-back.js';
 import { GenerateAuthTokenUseCase } from '../../application/usecases/generate-auth-token.js';
-import { TokenMongoDBRepository } from '../database/mongodb/repositories/token.repository.js';
+import { TokenRedisRepository } from '../database/redis/repositories/token.repository.js';
 import { CreateTokenUseCase } from '../../application/usecases/create-token.js';
 import { GetVideosFromPlaylistUseCase } from '../../application/usecases/get-videos-from-playlist.js';
 import { GetPartnerBearerTokenUseCase } from '../../application/usecases/get-bearer-token.js';
 import { SaveRefreshTokenUseCase } from '../../application/usecases/save-refresh-token.js';
 import { YoutubeService } from '../services/youtube/youtube.service.js';
 import { GoogleAuthService } from '../services/google-auth/google-auth.service.js';
-import { connectMongoose } from '../database/mongodb/connection.js';
 import { GetPlaylistsUseCase } from '../../application/usecases/get-playlists.js';
 import { GetVideoUseCase } from '../../application/usecases/get-video-use-case.js';
 import { GetSubscribedChannelsUseCase } from '../../application/usecases/get-subscribed-channels.js';
@@ -20,11 +19,25 @@ import { SaveUserUseCase } from '../../application/usecases/save-user-use-case.j
 import { UserMongoDBRepository } from '../database/mongodb/repositories/user.repository.js';
 import { JwtService } from '../auth/jwt.js';
 import { UpdateUserUseCase } from '../../application/usecases/update-user-use-case.js';
+import { connectMongoose } from '../database/mongodb/connection.js';
+import { connectRedis } from "../database/redis/connection.js";
 
-await connectMongoose();
+try {
+  await connectMongoose();
+} catch (err) {
+  console.error('Erro ao conectar ao MongoDB:', err);
+  throw err;
+}
+
+try {
+  await connectRedis();
+} catch (err) {
+  console.error('Erro ao conectar ao Redis:', err);
+  throw err;
+}
 
 /* repositories */
-const tokenMongoDBRepository = new TokenMongoDBRepository();
+const tokenRedisDBRepository = new TokenRedisRepository();
 const userMongoDBRepository = new UserMongoDBRepository();
 
 /* services */
@@ -43,16 +56,16 @@ const saveUserUseCase = new SaveUserUseCase(
 );
 const saveRefreshTokenUseCase = new SaveRefreshTokenUseCase(
   googleAuthService,
-  tokenMongoDBRepository,
+  tokenRedisDBRepository,
 );
 const getPartnerBearerTokenUseCase = new GetPartnerBearerTokenUseCase(
   saveRefreshTokenUseCase,
-  tokenMongoDBRepository,
+  tokenRedisDBRepository,
   authService,
   userMongoDBRepository,
 );
 const createToken = new CreateTokenUseCase(
-  tokenMongoDBRepository,
+  tokenRedisDBRepository,
 );
 const generateAuthToken = new GenerateAuthTokenUseCase(
   googleAuthService,
