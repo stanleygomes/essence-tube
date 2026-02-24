cc24653d4cbe62174b1357f03938313eac1676e8
stanley <stanleygomesdasilva@gmail.com>
2026-02-24 06:35:22 -0300

    chore: corrigir prettier

--- apps/ui/src/shared/services/http-client.ts
+++ apps/ui/src/shared/services/http-client.ts
@@ -23,61 +23,39 @@ export class HttpClient {
 
   async get<T = any>(url: string, config?: any): Promise<T> {
     const headers = { ...this.getAuthHeaders(), ...(config?.headers || {}) };
-    const response: any = await axios.get(
-      `${this.baseUrl}${url}`,
-      {
-        ...config,
-        headers,
-      },
-    );
+    const response: any = await axios.get(`${this.baseUrl}${url}`, {
+      ...config,
+      headers,
+    });
 
     return response.data;
   }
 
-  async post<T = any>(
-    url: string,
-    data?: any,
-    config?: any,
-  ): Promise<T> {
+  async post<T = any>(url: string, data?: any, config?: any): Promise<T> {
     const headers = { ...this.getAuthHeaders(), ...(config?.headers || {}) };
-    const response: any = await axios.post(
-      `${this.baseUrl}${url}`,
-      data,
-      {
-        ...config,
-        headers,
-      },
-    );
+    const response: any = await axios.post(`${this.baseUrl}${url}`, data, {
+      ...config,
+      headers,
+    });
 
     return response.data;
   }
 
-  async put<T = any>(
-    url: string,
-    data?: any,
-    config?: any,
-  ): Promise<T> {
+  async put<T = any>(url: string, data?: any, config?: any): Promise<T> {
     const headers = { ...this.getAuthHeaders(), ...(config?.headers || {}) };
-    const response: any = await axios.put(
-      `${this.baseUrl}${url}`,
-      data,
-      {
-        ...config,
-        headers,
-      },
-    );
+    const response: any = await axios.put(`${this.baseUrl}${url}`, data, {
+      ...config,
+      headers,
+    });
     return response.data;
   }
 
   async delete<T = any>(url: string, config?: any): Promise<T> {
     const headers = { ...this.getAuthHeaders(), ...(config?.headers || {}) };
-    const response: any = await axios.delete(
-      `${this.baseUrl}${url}`,
-      {
-        ...config,
-        headers,
-      },
-    );
+    const response: any = await axios.delete(`${this.baseUrl}${url}`, {
+      ...config,
+      headers,
+    });
     return response.data;
   }
 }
